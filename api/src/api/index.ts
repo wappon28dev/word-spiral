d2f1182d577fcedfee5f6bdc51b519fb6ab449fd
wappon28dev <wappon_28_dev@ruru.be>
2023-11-15 10:24:20 +0900

    💥 API 類削除

--- api/src/api/index.ts
+++
@@ -1,5 +0,0 @@
-import { createHono } from "lib/constant";
-import { cors } from "hono/cors";
-import { rooms } from "./rooms";
-
-export const v1 = createHono().use("*", cors()).route("/rooms", rooms);
--- api/src/types/util.ts
+++
@@ -1,5 +0,0 @@
-export type ElementType<T extends unknown[]> = T extends Array<infer U>
-  ? U
-  : never;
-
-export type OmitStrict<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
